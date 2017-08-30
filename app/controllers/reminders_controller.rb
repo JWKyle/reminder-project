525c107196306dbca6b374650374bf2ece42f4e6
George <georgedzuricsko1@gmail.com>
2017-08-30 16:58:38 -0500

    fixed error handling with form_with

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -3,17 +3,34 @@ class RemindersController < ApplicationController
     @reminder = Reminder.find(params[:id])
   end
 
+  def new
+    @reminder = Reminder.new
+  end
+
   def create
     @reminder = Reminder.new(reminder_params)
-    # @reminder.author = current_user
-    @reminder.save
-    # adapt redirect to @user after merge
-    redirect_to @reminder
+    @reminder.author_id = 1
+    if @reminder.save
+      p "succeeded!"
+      redirect_to @reminder, notice: 'reminder was successfully created.'
+    else
+      p "failed!!"
+      render :new, notice: 'reminder failed.'
+    end
   end
 
   def edit
     @reminder = Reminder.find(params[:id])
+  end
 
+  def update
+    @reminder = Reminder.find(params[:id])
+
+    if @reminder.update(params[:reminder].permit(:text, :send_at))
+      redirect_to @reminder
+    else
+      render 'edit'
+    end
   end
 
   def destroy
