3b22abc8e95f929c009d6028c93cc186e108e52b
Jim Kyle <jim@jwkyle.com>
2017-08-31 11:38:34 -0500

    add tests for create user fails, tests green

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,18 +24,58 @@ describe UsersController do
     it "responds with status code 302" do
       # user_params = FactoryGirl.attributes_for(:user)
       # post :create, { user => user_params}
-      post :create, user: { first_name: "Test", last_name: "Tester", email: "test@online.com", phone: 8475309, password: 12345 }
+      post :create, params: {user: { first_name: "Test", last_name: "Tester", email: "test@online.com", phone: 8475309, password: 12345 }}
       expect(response).to have_http_status 302
     end
 
     it "assigns a new user to @user" do
-      post 'sessions/create', user: { first_name: "Test", last_name: "Tester", email: "test@online.com", phone: 8475309, password: 12345 }
-      expect(assigns(:user)).to be_a_new User
+      post :create, params: {user: { first_name: "Test", last_name: "Tester", email: "test@online.com", phone: 8475309, password: 12345 }}
+      expect(assigns(:user)).to be_a_kind_of User
     end
 
-    # it "renders the :show template" do
-    #   get :new
-    #   expect(response).to render_template(:new)
-    # end
+    it "renders the :show template" do
+      post :create, params: {user: { first_name: "Test", last_name: "Tester", email: "test@online.com", phone: 8475309, password: 12345 }}
+      expect(response).to redirect_to assigns(:user)
+    end
+  end
+
+  context "when invalid params are passed" do
+    it "responds with status code 422: Unprocessable Entity" do
+      post :create, params: {user: { first_name: "", last_name: "Tester", email: "", phone: 8475309, password: "" }}
+      expect(response).to have_http_status 422
+    end
+
+    it "does not create a new game in the database" do
+      expect { post :create, params: {user: { first_name: "", last_name: "Tester", email: "", phone: 8475309, password: "" }}}.to_not change(User, :count)
+    end
+
+    it "assigns the unsaved user as @user" do
+      post :create, params: {user: { first_name: "", last_name: "Tester", email: "", phone: 8475309, password: "" }}
+      expect(assigns(:user)).to be_a(User)
+    end
+
+    it "renders the :new template" do
+      post :create, params: {user: { first_name: "", last_name: "Tester", email: "", phone: 8475309, password: "" }}
+      expect(response).to render_template(:new)
+    end
+  end
+
+  context "when invalid params are passed" do
+    describe "GET #show" do
+      it "responds with status code 200" do
+        get :show, params: { id: user.id}
+        expect(response).to have_http_status 200
+      end
+
+      it "assigns a show user to @user" do
+        get :show
+        expect(assigns(:user)).to be_a User
+      end
+
+      it "renders the :show template" do
+        get :show
+        expect(response).to render_template(:show)
+      end
+    end
   end
 end
