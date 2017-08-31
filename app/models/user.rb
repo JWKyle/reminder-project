765b6b2254201e45511f4569c51b8a324c3bafdc
greghubdub <gregnagy10@gmail.com>
2017-08-31 16:41:52 -0500

    Uncomment associations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
   validates :first_name, :last_name, :email, :phone, :password_digest, presence: true
+
+  has_many :reminders, foreign_key: :author_id
+
 end
