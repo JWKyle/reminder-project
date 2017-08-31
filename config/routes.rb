c6b798bf1d672d21e02b6179469730ee562a3930
greghubdub <gregnagy10@gmail.com>
2017-08-31 17:12:24 -0500

    Tweak Reminder model for Twilio

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :reminders
 
-  post 'twilio/message' => 'twilio#message'
+  # post 'twilio/message' => 'twilio#message'
 
   root 'sessions#new'
 end
