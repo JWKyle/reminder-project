1e110b52c7ec04d913a9cfe243a0c76edda3a72c
greghubdub <gregnagy10@gmail.com>
2017-08-31 16:33:23 -0500

    Add twilio controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :reminders
-  # root to: 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :reminders
+
+  post 'twilio/message' => 'twilio#message'
 
   root 'sessions#new'
 end
