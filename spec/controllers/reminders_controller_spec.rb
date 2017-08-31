8c7f577f29b2a22c7cabfe3df6e5adf230aa397b
greghubdub <gregnagy10@gmail.com>
2017-08-31 10:43:20 -0500

    Write tests for reminder#show controller

--- spec/controllers/reminders_controller_spec.rb
+++ spec/controllers/reminders_controller_spec.rb
@@ -8,6 +8,17 @@ describe RemindersController do
 			get :show, { id: reminder.id }
 			expect(response).to have_http_status 200
 		end
+
+		it "assigns the correct reminder as @reminder" do
+			get :show, { id: response.id }
+			expect(assigns(:reminder)).to eq(reminder)
+		end
+
+		it "renders the :show template" do
+			get :show, { id: reminder.id }
+			expect(response).to render_template(:show)
+		end
+
 	end
 
 
