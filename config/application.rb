41a174de3318d2f8b3b0ab5f81c3fe94ea2476fa
Jim Kyle <jim@jwkyle.com>
2017-09-01 15:34:31 -0500

    edit config line, works on heroku now

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,10 @@ module ReminderApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.active_job:queue_adapter = :delayed_job
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_job.queue_adapter = :delayed_job
   end
 end
